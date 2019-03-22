b0b7c56b2f520cc293c51086722be784290b170f
akkalbist55 <akkalbist55@gmail.com>
2019-03-22 23:15:33 +0545

    Main function committed by Akkal

---
+++ main_function.py
@@ -0,0 +1,23 @@
+
+def main():
+    print("Hello Python Programmer,")
+
+#here is not define main function
+
+print("You are Welcome to Programming World!")
+print("<--------------------------------------->")
+
+def main():
+    print("Hello Python Programmer")
+#here is define main function
+
+if __name__ == "__main__":
+    main()
+
+print("You are Welcome to Programming World!")
+print("______________________________________")
+
+#Once you define the main function, It well call the main function and display Hello Python Programmer
+if __name__ == "__main__":
+    main()
+
